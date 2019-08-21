3ecca7a83bdbd8e5d5a0fbb3e567512afd627903
jhony maurad <jhonymaurad@gmail.com>
2019-08-21 19:31:23 -0400

    Set up Express server and Google Auth 2.0

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'thereeeeeeeeeeee' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//http://localhost:5000/auth/google/callback
