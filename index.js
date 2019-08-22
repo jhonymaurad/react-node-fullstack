5c21f7c8ad7fe5e8cae6bf18d0d44ebdefcd9a31
jhony maurad <jhonymaurad@gmail.com>
2019-08-21 23:07:42 -0400

    Install mongoose and initial setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
