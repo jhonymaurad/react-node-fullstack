d10757028152b245fe16359e6cd1bb86a759796c
jhony maurad <jhonymaurad@gmail.com>
2019-08-24 12:13:02 -0400

    Enabled cookies

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,4 +7,7 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
