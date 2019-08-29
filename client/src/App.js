29e5c5eb953977a6e7e73d794ccf7c04870482f5
jhony maurad <jhonymaurad@gmail.com>
2019-08-28 20:17:01 -0400

    Added front end with create react app and refactored passport.js to use async await

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      <a href="/auth/google">Sign in with Google</a>
+    </div>
+  );
+}
+
+export default App;
