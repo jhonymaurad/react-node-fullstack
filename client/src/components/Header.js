9a7cea51d955f3209feb2bb5661f5565f211e0af
jhony maurad <jhonymaurad@gmail.com>
2019-09-03 22:24:02 -0400

    Finished Header component and Added Link to the logo, began setting keys for stripe configuration

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Log in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <Link
+            className="left brand-logo"
+            to={this.props.auth ? '/surveys' : '/'}
+          >
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
