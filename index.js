c2f68ea010053668ed24bcc5c58a84ae6fcf7a6b
jhony maurad <jhonymaurad@gmail.com>
2019-09-08 21:38:33 -0400

    Create action for payment token and updated backend to handle post request / Installed stripe and body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,8 +22,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const authRoutes = require('./routes/authRoutes');
+const billingRoutes = require('./routes/billingRoutes');
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
