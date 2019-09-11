a8d452cec3676cdf10d8534838580c79cf2835c1
jhony maurad <jhonymaurad@gmail.com>
2019-09-11 14:47:04 -0400

    Added billing and client side app!

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.use(passport.session());
 const authRoutes = require('./routes/authRoutes');
 const billingRoutes = require('./routes/billingRoutes');
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+  // Express will serve up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 authRoutes(app);
 billingRoutes(app);
 
