746e7be0705edfea1839be9b534f23f6f6cb25e6
jhony maurad <jhonymaurad@gmail.com>
2019-09-27 17:12:33 -0400

    Added validation to the form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,12 @@
 import React from 'react';
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input}></input>
+      <input {...input} style={{ marginBottom: '5px' }}></input>
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,50 +1,64 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
         <Field
-          type="text"
-          name="title"
+          key={name}
           component={SurveyField}
-          label="Survey Title"
-        />
-        <Field
           type="text"
-          name="subject"
-          component={SurveyField}
-          label="Subject Line"
+          label={label}
+          name={name}
         />
-        <Field
-          type="text"
-          name="body"
-          component={SurveyField}
-          label="Email Body"
-        />
-        <Field
-          type="text"
-          name="emails"
-          component={SurveyField}
-          label="Recipient List"
-        />
-      </div>
-    );
+      );
+    });
   }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link className="red btn-flat white-text " to="/surveys">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons rigth">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = `you must providea value`;
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate: validate,
   form: 'surveyForm'
 })(SurveyForm);
